9ddb0a6bd1ff5a5dda0900934acf9ebab50cb91b
kolen <incredible.angst@gmail.com>
2023-03-03 04:14:31 +0300

    Configure proxying to dev backend

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+    plugins: [svelte()],
+    server: {
+        proxy: {
+            "/api": "http://127.0.0.1:8080",
+            "/auth": "http://127.0.0.1:8080",
+        },
+    }
 })
